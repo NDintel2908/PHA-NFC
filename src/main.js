7ecc255163dd0c073d4a73b54f594f247bdc07c5
bao <nvquocbao6@gmail.com>
2023-11-08 19:18:50 +0700

    FeAT: ADD VUTIFY

--- src/main.js
+++ src/main.js
@@ -2,13 +2,21 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 import Layout from './layout.vue'
 import router from './router'
 
-const layout = createApp(Layout)
+const app = createApp(Layout)
 
-layout.use(createPinia())
-layout.use(router)
+app.use(createPinia()) // Use Pinia for state management
+app.use(router) // Use the router
+const vuetify = createVuetify({
+  components,
+  directives
+})
+app.use(vuetify) 
 
-layout.mount('#layout')
+app.mount('#layout')
--- src/pages/auth/login.vue
+++ src/pages/auth/login.vue
@@ -1,79 +1,11 @@
 <template>
-    <div class="login-container">
-      <h2>Login</h2>
-      <form @submit.prevent="login">
-        <div class="form-group">
-          <label for="username">Username</label>
-          <input type="text" v-model="username" class="form-control" id="username" required>
-        </div>
-        <div class="form-group">
-          <label for="password">Password</label>
-          <input type="password" v-model="password" class="form-control" id="password" required>
-        </div>
-        <button type="submit" class="btn btn-primary">Login</button>
-      </form>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        username: '',
-        password: ''
-      };
-    },
-    methods: {
-      login() {
-        // Đưa logic xác thực ở đây
-        // Ví dụ:
-        if (this.username === 'admin' && this.password === 'password') {
-          alert('Đăng nhập thành công!');
-        } else {
-          alert('Sai tên đăng nhập hoặc mật khẩu. Vui lòng thử lại.');
-        }
-      }
-    }
-  };
-  </script>
-  
-  <style scoped>
-  .login-container {
-    max-width: 400px;
-    margin: auto;
-    padding: 20px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    background-color: #fff;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  }
-  
-  .form-group {
-    margin-bottom: 15px;
-  }
-  
-  .form-control {
-    width: 100%;
-    padding: 10px;
-    font-size: 16px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-  }
-  
-  .btn-primary {
-    display: block;
-    width: 100%;
-    padding: 10px;
-    font-size: 16px;
-    background-color: #007bff;
-    border: none;
-    border-radius: 4px;
-    color: #fff;
-    cursor: pointer;
-  }
-  
-  .btn-primary:hover {
-    background-color: #0056b3;
-  }
-  </style>
-  
+  <v-text-field placeholder="213"></v-text-field>
+</template>
+
+<script setup>
+const form = ref({
+  email: '',
+  password: '',
+  rememberMe: false,
+})
+</script>
